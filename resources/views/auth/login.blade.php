4b68cb4e4b14bd493bcb4a5c5bd3ccbc4b4a113f
Alan <alan.mcfarlane@gmail.com>
2023-03-04 17:13:29 +0000

    Added password toggles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,13 @@
                 <div class="flex flex-col space-y-1">
                     <label for="password" class="font-medium text-sm text-slate-600">Password</label>
                     <div class="flex flex-col space-y-0.5 w-full">
-                        <input id="password" type="password" name="password" class="border border-slate-300 text-sm rounded px-2.5 focus:outline-none focus:border-blue-300 focus:ring-1 focus:ring-blue-300" autocomplete="current-password" required />
+                        <div class="flex border border-slate-300 rounded focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 overflow-hidden" x-data="{visible:false}">
+                            <input id="password" :type="visible?'text':'password'" name="password" class="border-none focus:border-none text-sm px-2.5 focus:outline-none flex-grow" autocomplete="current-password" required x-ref="input" />
+                            <button type="button" tabindex="-1" class="p-2 bg-neutral-100 border-l border-slate-300 text-slate-500 focus:outline-none" @click.stop.prevent="visible=!visible;$refs.input.focus()">
+                                <x-heroicons.outline.eye class="w-5 h-5" x-cloak x-show="visible" />
+                                <x-heroicons.outline.eye-slash class="w-5 h-5" x-show="!visible" />
+                            </button>
+                        </div>
                         @error('password')
                             <span class="text-sm text-red-500">{{ $message }}</span>
                         @enderror
--- resources/views/partials/navbar/auth.blade.php
+++ resources/views/partials/navbar/auth.blade.php
@@ -8,8 +8,8 @@
                 <button type="button" class="h-14 flex space-x-1 items-center px-5 text-slate-600 hover:bg-slate-100 focus:bg-slate-100 focus:outline-none" @click.prevent.stop="open=!open">
                     <span>{{ Auth::user()->name }}</span>
                     <span class="opacity-75">
-                        <x-heroicons.mini.chevron-down x-show="!open" />
-                        <x-heroicons.mini.chevron-up x-cloak x-show="open" />
+                        <x-heroicons.mini.chevron-down x-show="!open" class="w-5 h-5" />
+                        <x-heroicons.mini.chevron-up x-cloak x-show="open" class="w-5 h-5" />
                     </span>
                 </button>
                 <div class="absolute right-0 top-0 mt-16 bg-white whitespace-nowrap flex flex-col py-2 rounded shadow" @click.prevent.away="open=false" x-show="open" x-cloak>
