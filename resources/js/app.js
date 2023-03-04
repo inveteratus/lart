85ccb683ef2b1c22834611fcfea16deccc7fe6df
Alan <alan.mcfarlane@gmail.com>
2023-03-04 16:39:01 +0000

    Added AlpineJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import Alpine from 'alpinejs'
+import persist from '@alpinejs/persist'
+import focus from '@alpinejs/focus'
+
+window.Alpine = Alpine
+
+Alpine.plugin(persist)
+Alpine.plugin(focus)
+Alpine.start()
