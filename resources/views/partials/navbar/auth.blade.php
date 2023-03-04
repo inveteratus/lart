4ba66e8458daa1c5ed2c637fc22dfcd2da07b3b4
Alan <alan.mcfarlane@gmail.com>
2023-03-04 16:54:51 +0000

    Moved heroicons to components

--- resources/views/partials/navbar/auth.blade.php
+++ resources/views/partials/navbar/auth.blade.php
@@ -8,12 +8,8 @@
                 <button type="button" class="h-14 flex space-x-1 items-center px-5 text-slate-600 hover:bg-slate-100 focus:bg-slate-100 focus:outline-none" @click.prevent.stop="open=!open">
                     <span>{{ Auth::user()->name }}</span>
                     <span class="opacity-75">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5" data-source="heroicons.mini.chevron-down" x-cloak x-show="!open">
-                            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5" data-source="heroicons.mini.chevron-up" x-cloak x-show="open">
-                            <path fill-rule="evenodd" d="M14.77 12.79a.75.75 0 01-1.06-.02L10 8.832 6.29 12.77a.75.75 0 11-1.08-1.04l4.25-4.5a.75.75 0 011.08 0l4.25 4.5a.75.75 0 01-.02 1.06z" clip-rule="evenodd" />
-                        </svg>
+                        <x-heroicons.mini.chevron-down x-show="!open" />
+                        <x-heroicons.mini.chevron-up x-cloak x-show="open" />
                     </span>
                 </button>
                 <div class="absolute right-0 top-0 mt-16 bg-white whitespace-nowrap flex flex-col py-2 rounded shadow" @click.prevent.away="open=false" x-show="open" x-cloak>
