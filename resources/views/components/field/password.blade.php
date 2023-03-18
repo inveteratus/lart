0cad7919a9777757c860ec13f6a2ecff9b8c8065
Alan <alan.mcfarlane@gmail.com>
2023-03-18 18:12:11 +0000

    Changed to bootstrap icons

--- resources/views/components/field/password.blade.php
+++ resources/views/components/field/password.blade.php
@@ -7,8 +7,8 @@
         <div class="flex border border-slate-300 rounded focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 overflow-hidden" x-data="{visible:false}">
             <input :id="$id('field')" name="{{ $name }}" :type="visible?'text':'password'" {{ $attributes->merge(['class' => "border-none focus:border-none text-sm px-2.5 focus:outline-none flex-grow"]) }} x-ref="input" />
             <button type="button" tabindex="-1" class="p-2 bg-neutral-100 border-l border-slate-300 text-slate-500 focus:outline-none" @click.stop.prevent="visible=!visible;$refs.input.focus()">
-                <x-heroicons.outline.eye class="w-5 h-5" x-cloak x-show="visible" />
-                <x-heroicons.outline.eye-slash class="w-5 h-5" x-show="!visible" />
+                <x-icons.eye class="w-5 h-5" x-cloak x-show="visible" />
+                <x-icons.eye-slash class="w-5 h-5" x-show="!visible" />
             </button>
         </div>
 
--- resources/views/partials/navbar/auth.blade.php
+++ resources/views/partials/navbar/auth.blade.php
@@ -8,19 +8,19 @@
                 <button type="button" class="h-14 flex space-x-1 items-center px-5 text-slate-600 hover:bg-slate-100 focus:bg-slate-100 focus:outline-none" @click.prevent.stop="open=!open">
                     <span>{{ Auth::user()->name }}</span>
                     <span class="opacity-75">
-                        <x-heroicons.mini.chevron-down x-show="!open" class="w-5 h-5" />
-                        <x-heroicons.mini.chevron-up x-cloak x-show="open" class="w-5 h-5" />
+                        <x-icons.caret-down x-show="!open" class="w-5 h-5" />
+                        <x-icons.caret-up x-cloak x-show="open" class="w-5 h-5" />
                     </span>
                 </button>
                 <div class="absolute right-0 top-0 mt-16 bg-white whitespace-nowrap flex flex-col py-2 rounded shadow z-10" @click.prevent.away="open=false" x-show="open" x-cloak>
                     <a href="{{ route('settings') }}" class="px-5 py-1 text-slate-600 hover:bg-slate-100 focus:bg-slate-100 focus:outline-none inline-flex items-center space-x-2">
-                        <x-heroicons.outline.cog-6-tooth class="w-5 h-5 opacity-75" />
+                        <x-icons.gear class="w-5 h-5 opacity-75" />
                         <span>Settings&hellip;</span>
                     </a>
                     <hr class="my-1.5">
                     <form action="{{ route('logout') }}" method="post">
                         <button type="submit" class="px-5 py-1 hover:bg-slate-100 w-full text-slate-700 text-left focus:bg-slate-100 focus:outline-none inline-flex items-center space-x-2">
-                            <x-heroicons.outline.power class="w-5 h-5 opacity-75" />
+                            <x-icons.power class="w-5 h-5 opacity-75" />
                             <span>Logout</span>
                         </button>
                         @csrf
