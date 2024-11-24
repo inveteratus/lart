1790ce16a98b192d80af65171954b6be68e437a7
Inveteratus <alan.mcfarlane@gmail.com>
2024-11-24 06:26:23 +0000

    Added livewire

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+
+// Register any Alpine directives, components, or plugins here...
+
+Livewire.start()
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
+        @livewireStyles
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @else
@@ -172,5 +173,6 @@
                 </div>
             </div>
         </div>
+        @livewireScriptConfig
     </body>
 </html>
